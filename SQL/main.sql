7fea7ce1330c62a2bf17c5900a713ca767faa1f1
Prthm-Buggy <prathamtalele101@gmail.com>
2025-07-15 21:15:24 +0530

    commit

---
+++ SQL/main.sql
@@ -0,0 +1,46 @@
+
+--select * from employees;
+
+
+SELECT first_name, COALESCE(manager_id, 'N/A') AS last_name_display
+FROM employees limit 5;
+
+
+select * from books;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
